4a33eec81bb337697416f7fd595f2701b33513e0
lizhongze <lizhongze.chn@gmail.com>
2019-12-05 17:47:12 +0800

    更新：增加 vuex 插件

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import Vue from 'vue'
-import './plugins/axios'
-import App from './App.vue'
-import router from './router/'
-import './plugins/element.js'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router/';
+import store from './store';
+import './plugins/axios';
+import './plugins/element.js';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app');
