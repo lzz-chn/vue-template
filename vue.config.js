93ca667e7fe4a84dd41b8022d08726bcd4e11e3b
lizhongze <lizhongze.chn@gmail.com>
2019-12-06 11:36:17 +0800

    更新：增加配置 proxy 代理解决跨域问题

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,24 @@
 module.exports = {
-    publicPath: './', // 使用相对路径打包
-    assetsDir: 'static', // 静态资源目录名称
-    outputDir:"dist", // 打包资源导出文件路径
+    publicPath: process.env.VUE_APP_PUBLIC_PATH,
+    // outputDir: process.env.outputDir || 'dist', // 'dist', 生产环境构建文件的目录
+    // assetsDir: '', // 相对于outputDir的静态资源(js、css、img、fonts)目录
+    // runtimeCompiler: true, // 是否使用包含运行时编译器的 Vue 构建版本
     configureWebpack: {
         devtool: 'source-map'
     },
-    productionSourceMap: false,  // 生产环境是否生成 sourceMap 文件
+    productionSourceMap: false, // 生产环境是否生成 sourceMap 文件
     pluginOptions: {},
     devServer: {
-        disableHostCheck: true  // 允许内网穿透
+        // disableHostCheck: true, // 允许内网穿透
+        // proxy: {
+        //     '/api': {
+        //         target: 'https://www.easy-mock.com/mock/5bc75b55dc36971c160cad1b/sheets', // 目标代理接口地址
+        //         secure: false,
+        //         changeOrigin: true, // 开启代理，在本地创建一个虚拟服务端支持跨域
+        //         pathRewrite: {
+        //             '^/api': '/'  // 重写接口路径，使用 '/' 代替 '/api'
+        //         }
+        //     }
+        // }
     }
-}
+};
