2d6061044e3102b806df1bfb55fd95a0ea912727
lizhongze <lizhongze.chn@gmail.com>
2020-08-12 12:03:14 +0800

    更新：上传文件组件增加文件大小限制、选择上传文件时的文件类型过滤、增加'ALL'文件类型

--- src/components/element/fileUpload.vue
+++ src/components/element/fileUpload.vue
@@ -16,6 +16,7 @@
             :file-list="fileList"
             :limit="1"
             :on-exceed="handleExceed"
+            :accept="accept"
         >
             <i class="el-icon-upload"></i>
             <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
@@ -32,23 +33,48 @@ export default {
                 scene: 'default',
                 output: 'json'
             },
-            filePostfix: ''
+            filePostfix: '',
+            accept: ''
         }
     },
     props: { fileType: { type: String, default: 'null' }, fileList: { type: Array, default: [] } },
+    watch: {
+        fileType(val) {
+            switch (val) {
+                case 'PPT':
+                    this.accept = '.pptx,.pptm,.ppt,.potx,.potm,.pot,.ppsx,.ppsm,.pps,.ppam,.ppa'
+                    break
+                case 'PDF':
+                    this.accept = '.pdf'
+                    break
+                case 'VIDEO':
+                    this.accept = '.avi,.wmv,.mpeg,.mp4,.m4v,.mov,.asf,.flv,.f4v,.rmvb,.rm,.3gp,.vob'
+                    break
+            }
+        }
+    },
+    created() {
+        // 拦截演示账号的上传请求
+        if (sessionStorage.getItem('userId') == process.env.VUE_APP_DEMO_ID) {
+            this.actionUrl += '/mock'
+        }
+    },
     methods: {
         // 过滤文件格式
         filtersFile(file) {
             let isType
             switch (this.fileType) {
-                case 'ppt':
-                    isType = file.name.match(/.(pptx|pptm|ppt|potx|potm|pot|ppsx|ppsm|pps|ppam|ppa})$/g)
+                case 'PPT':
+                    isType = file.name.match(/.(pptx|pptm|ppt|potx|potm|pot|ppsx|ppsm|pps|ppam|ppa)$/g)
                     break
-                case 'pdf':
+                case 'PDF':
                     isType = file.name.match(/.pdf$/g)
                     break
-                case 'video':
-                    isType = file.name.match(/.(avi|wmv|mpeg|mp4|m4v|mov|asf|flv|f4v|rmvb|rm|3gp|vob})$/g)
+                case 'VIDEO':
+                    isType = file.name.match(/.(avi|wmv|mpeg|mp4|m4v|mov|asf|flv|f4v|rmvb|rm|3gp|vob)$/g)
+                    break
+                case 'ALL':
+                    isType = true
                     break
             }
 
@@ -58,23 +84,22 @@ export default {
         beforeUpload(file) {
             let isType = this.filtersFile(file)
             let isUpload = false
-            // let isLtSize = file.size / 1024 / 1024 < 10 // 小于 size M
+            let isLtSize = file.size / 1024 / 1024 < 10 // 小于 size M
+
             if (this.fileType == 'null') {
                 this.$message.error('请选择上传文件的类别!')
                 return isUpload
             }
             if (!isType) {
                 this.$message.error('请上传已选择的文件类别!')
-            } else {
-                isUpload = true
+            }
+            if (!isLtSize) {
+                this.$message.error(`上传文件大小不能超过 10MB!`)
+            }
+            if (isType && isLtSize) {
+                this.isUpload = true
                 this.filePostfix = isType[0]
             }
-            // if (!isLtSize) {
-            //     this.$message.error(`上传图片大小不能超过 10MB!`)
-            // }
-            // if (isType && isLtSize) {
-            //     this.isUpload = true
-            // }
             return isUpload
         },
         // 上传成功
